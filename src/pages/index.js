6c8f88002ec7f46f491a8805928dd9d3e7473e46
Andy Pearson <andy@londonparkour.com>
2022-06-05 08:34:40 +0100

    full height

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,34 @@ const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
-      <h1 className="text-1xl text-blue-500">
-        <span>SVG Converter </span>
-        <span role="img" aria-label="Party popper emojis">
-          ⚙️
-        </span>
-      </h1>
+      
+      <div className="w-full h-screen flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
+
+        <div className="w-fixed w-1/6 flex-shrink flex-grow-0 px-4 bg-slate-100">
+            <div className="sticky top-0 p-4 w-full h-full">
+                NAV
+            </div>
+        </div>
+
+
+        <div role="main" className="w-4/6 h-full flex-grow pt-1 px-3">
+          CONTENT
+        </div>
+        
+        <div className="w-fixed w-1/6 flex-shrink flex-grow-0 px-2 bg-slate-200">
+            <div className="flex sm:flex-col px-2">
+                SIDEBAR
+            </div>
+        </div>
+      </div>
+
+
+      <footer className="bg-slate-300 mt-auto">
+        FOOTER
+      </footer>
       
     </main>
+
   )
 }
 
