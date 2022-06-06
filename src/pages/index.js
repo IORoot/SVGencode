5b7812ee2f3fb4abc5b232e3a5f50682b4f6f274
Andy Pearson <andy@londonparkour.com>
2022-06-06 18:14:21 +0100

    title

--- src/pages/index.js
+++ src/pages/index.js
@@ -146,21 +146,23 @@ class IndexPage extends React.Component {
 
     return (
       <main>
-        <title>Home Page</title>
+        <title>SVG Encoder</title>
         
         <div className="w-full min-h-screen flex sm:flex-row">
 
           <div className="w-fixed w-1/6 flex-shrink flex-grow-0 p-4 bg-slate-100">
               <div className="sticky top-0 p-4 w-full h-full flex flex-col gap-8">
-                  <div className="">SVG Encode ⚙️</div>
+                  <h1 className="">SVG Encode ⚙️</h1>
 
-                  <p className="text-sm">
+                  <h2 className="text-sm">
                     A simple tool to convert an SVG into different formats, ready for use in CSS and HTML.
-                  </p>
+                  </h2>
 
                   <a className="hover:fill-blue-800 fill-black" href="https://github.com/IORoot/SVGconverter">
                     <svg role="img" width="2rem" height="2rem" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
                   </a>
+
+                  <p className="text-xxs mt-auto">Created by Andy Pearson (IORoot) 2022.</p>
               </div>
           </div>
 
@@ -172,12 +174,12 @@ class IndexPage extends React.Component {
               
               {/* Image */}
               <div className="col-span-1 flex sm:flex-col px-2">
-                  <h2 className="mb-2 text-sm font-medium text-gray-900">SVG Preview</h2>
+                  <h3 className="mb-2 text-sm font-medium text-gray-900">SVG Preview</h3>
                   <SvgImage className="w-full h-40 checkered overflow-scroll rounded">{this.state.svgRawCode}</SvgImage>
               </div>
 
 
-            <h2 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">URL-Encoding</h2>
+            <h3 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">URL-Encoding</h3>
 
               {/* URL Encoding without spaces */}
               <DumbTextarea className="col-span-1" label="URL-Encoded" rows="3" content={this.state.svgUrlregexEncoded}>Spaces excluded.</DumbTextarea>
@@ -197,7 +199,7 @@ class IndexPage extends React.Component {
 
 
 
-            <h2 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">Base64 Encoded</h2>
+            <h3 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">Base64 Encoded</h3>
 
               {/* Base64 Encoded */}
               <DumbTextarea className="col-span-1" label="Base64 Encoded" rows="3" content={this.state.svgBase64Encoded}>Single-quotes and Whitespace removed</DumbTextarea>
@@ -214,7 +216,7 @@ class IndexPage extends React.Component {
 
 
 
-            <h2 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">Alterations</h2>
+            <h3 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">Alterations</h3>
 
               {/* One-Liner */}
               <DumbTextarea className="col-span-1" label="One-liner" rows="2" content={this.state.svgRawCodeOneLine}>Removed newlines and extra whitespace</DumbTextarea>
@@ -237,7 +239,7 @@ class IndexPage extends React.Component {
 
 
 
-            <h2 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">Other Encodings</h2>
+            <h3 className="col-span-4 text-xl text-blue-900 border-b-2 border-blue-900">Other Encodings</h3>
 
               {/* Encode using encodeURI() */}
               <DumbTextarea className="col-span-1" label="Encode URI" rows="3" content={this.state.svgEncodedUri}>encodeURI(svg)</DumbTextarea>
@@ -255,7 +257,7 @@ class IndexPage extends React.Component {
         </div>
 
 
-        <div className="bg-slate-800 text-slate-100">
+        <div className="bg-slate-800 text-slate-100 text-right text-xxs p-4">
           <a href="https://github.com/IORoot/SVGconverter">Written by Andy Pearson (IORoot) 2022.</a>
         </div>
 
