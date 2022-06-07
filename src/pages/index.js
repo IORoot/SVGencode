ab33fb0e90d03519bc960393da49985a295ab8c1
Andy Pearson <andy@londonparkour.com>
2022-06-07 08:50:53 +0100

    errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -252,6 +252,7 @@ class IndexPage extends React.Component {
           </div>
 
           <div className="w-fixed w-1/6 flex-shrink flex-grow-0 p-4 bg-slate-200">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6307098859694609" crossorigin="anonymous"></script>
             <ins className="adsbygoogle"
                 style={{display: 'block'}}
                 data-ad-format="fluid"
