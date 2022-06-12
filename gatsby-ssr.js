a376b2241f6ab0d4e2b94650d81275270de5092d
Andy Pearson <andy@londonparkour.com>
2022-06-12 09:02:31 +0100

    remove adsense

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-const React = require('react')
-
-const HeadComponents = [
-    <script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6307098859694609" crossOrigin="anonymous" async />,
-]
-
-exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-    setHeadComponents(HeadComponents)
-}
